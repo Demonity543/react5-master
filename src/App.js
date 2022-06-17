4def71bdbc2b724dec4f140af8160223ee893a42
Tesl_ph26@student.itstep.org <Tesl_ph26@student.itstep.org>
2022-06-17 03:10:23 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState, useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(props){
+  const [dateNow, setDate] = useState(getTime())
+  const [seconds, setSeconds ] = React.useState(60);
+  const [timerActive, setTimerActive ] = React.useState(false);
+
+  React.useEffect(() => {
+    if (seconds > 0 && timerActive) {
+      setTimeout(setSeconds, 1000, seconds - 1);
+    } else {
+      setTimerActive(false);
+    }
+  }, [ seconds, timerActive ]);
+
+  useEffect( () => {
+    setInterval(()=>{
+      setDate(getTime())
+    },1000)
+
+  }, [])
+
+  function getTime(){
+    var date = new Date(),
+    hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+    minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+    seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    return {hours, minutes, seconds}
+  }
+  
+
+  return(
+    <div className="progress-bar">
+        <p>{dateNow.hours + ':' + dateNow.minutes + ':' + dateNow.seconds}</p>
+        <br></br>
+        <div>
+      {seconds
+        ? <React.Fragment>
+            <button id="start" onClick={() => setTimerActive(!timerActive)}>
+              {timerActive ? 'stop' : 'start'}
+            </button>
+            <input type={'range'} id="range" min={0} max={60} value={seconds} onChange={(e) => setSeconds(e.target.value)} />
+            <h3>{seconds}</h3>
+          </React.Fragment>
+        : <button onClick={() => setSeconds(60)}>Again</button>
+      }
+      <br></br>
     </div>
-  );
+    <div>
+      
+    </div>
+    </div>
+  )
 }
-
-export default App;
